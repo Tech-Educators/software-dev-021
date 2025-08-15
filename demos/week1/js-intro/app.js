a23749d69981c5ac86a6839249aa1c9ccd6b5c65
Joe Venton <joe.venton@techeducators.co.uk>
2025-08-15 08:31:24 +0100

    added flexbox, js-intro and positioning demos

---
+++ demos/week1/js-intro/app.js
@@ -0,0 +1,16 @@
+// let myNumber = 7;
+// myNumber = 5;
+// myNumber = 8675309;
+// console.log(myNumber);
+
+// const myNumber = 7;
+// console.log(myNumber);
+
+// let sum = 1 + 1;
+// console.log(sum);
+
+// let difference = 1 - 1;
+// console.log(difference);
+
+// let remainder = 5 % 2;
+// console.log(remainder);
