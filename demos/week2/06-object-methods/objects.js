c3c73d874f91f2d0dc9550b0633a22c84331d113
Manuel <manugonzalez.tech@gmail.com>
2025-08-19 15:55:34 +0100

    feat: add object methods demo

---
+++ demos/week2/06-object-methods/objects.js
@@ -0,0 +1,61 @@
+//objects
+//data containers for more complex data
+
+//structure
+// we use {} for objects
+//each property has two parts --> key: value
+let city = {
+  cityName: "Norwich",
+  population: 250000,
+  hasTrainStation: true,
+};
+
+console.log(city);
+
+//TODO: see the value of cityName in the console
+console.log(city.cityName); // "Norwich"
+
+//TODO: see the value of population in the console
+console.log(city.population);
+
+//TODO: Add a new property for country
+city.country = "UK";
+console.log(city);
+
+let newCity = {
+  cityName: "Kingston-Upon-Hull",
+  population: 250000,
+  hasTrainStation: true,
+  coffeeShops: ["Great Coffee", "Amazing Coffee", "Just Average Coffee"],
+  companies: [
+    { companyName: "Tech Educators", location: "some building" },
+    { companyName: "Tech Providers", location: "another building" },
+  ],
+};
+
+console.log(newCity.coffeeShops);
+console.log(newCity.coffeeShops[1]);
+
+//TODO: I want to see Tech Educators in the console
+console.log(newCity.companies[0].companyName);
+
+//===============================
+// object methods
+let thisCity = {
+  cityName: "Norwich",
+  population: 250000,
+  hasTrainStation: true,
+  saySlogan: function (visitor) {
+    console.log(`A fine city, welcome ${visitor}!`);
+  },
+};
+
+thisCity.saySlogan("Joe");
+
+//Examples of methods
+document.createElement("h1");
+document.getElementById("id");
+console.log("Hello world");
+array.forEach(function (item) {
+  console.log(item);
+});
