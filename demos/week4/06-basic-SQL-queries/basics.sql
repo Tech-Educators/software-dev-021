b4381c08ad3d4216c66c7341b7a1971d6fb2eafd
Manuel <manugonzalez.tech@gmail.com>
2025-09-02 13:50:44 +0100

    feat: add sql basics demo

---
+++ demos/week4/06-basic-SQL-queries/basics.sql
@@ -0,0 +1,30 @@
+-- Basic SQL queries
+
+-- create a table
+-- constraints: data type that we want to store in each column
+-- id column: represent each entry uniquely (PRIMARY KEY)
+CREATE TABLE staff (
+  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name TEXT,
+  location VARCHAR(255),
+  age INT,
+  role VARCHAR(255)
+);
+
+-- add new data to the table
+INSERT INTO staff (name, location, age, role)
+VALUES ('Manny', 'Norwich', 50, 'instructor'),
+('Tim', 'Norwich', 120, 'course director');
+
+-- select some data from my table
+-- when we select specific columns, we are not creating a new table. We are filtering the current table
+SELECT * FROM staff;
+
+SELECT name, location FROM staff;
+
+-- the keyword WHERE adds a condition to our SELECT
+SELECT * FROM staff WHERE name = 'Joe';
+
+
+
+
