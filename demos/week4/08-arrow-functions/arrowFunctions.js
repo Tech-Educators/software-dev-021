878187f46c2632b2e87bc110c4bd94d039799b02
Manuel <manugonzalez.tech@gmail.com>
2025-09-03 10:20:28 +0100

    feat: add new example to arrow functions demo

--- demos/week4/08-arrow-functions/arrowFunctions.js
+++ demos/week4/08-arrow-functions/arrowFunctions.js
@@ -22,3 +22,20 @@ array.forEach(function (item) {
 
 //arrow function
 array.forEach((item) => console.log(item));
+
+// event listener and handler
+function handleSubmit(event) {
+  event.preventDefault();
+  const username = event.target.username.value;
+  const message = event.target.message.value;
+
+  console.log({ username: username, message: message });
+}
+
+form.addEventListener("submit", handleSubmit);
+
+//arrow function
+
+const handleSubmit = (event) => {
+  //statements
+};
