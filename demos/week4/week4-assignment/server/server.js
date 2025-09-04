3ae03108cec83d2e26a1f319e89ae2f24d4a523c
Manuel <manugonzalez.tech@gmail.com>
2025-09-04 11:59:43 +0100

    feat: add week 4 assignment template

---
+++ demos/week4/week4-assignment/server/server.js
@@ -0,0 +1,16 @@
+//imports
+
+//initiliase express and cors
+
+//tell server to use JSON
+
+//set up a port
+
+//set up a root route
+
+//===============================================
+
+//TODO: I want to READ all data from my table
+
+//TODO: I want to CREATE new data in the table
+// - remember the body and the formValues
