161944190c067ab69df351ec3c2513e0a6c277c8
Manuel <manugonzalez.tech@gmail.com>
2025-09-24 09:54:17 +0100

    feat: add express server revision

---
+++ demos/week7/06-build-an-express-server/server.js
@@ -0,0 +1,25 @@
+//imports
+import express from "express";
+import cors from "cors";
+
+//initialise express
+const app = express();
+
+//config express with JSON
+//config cors in express
+app.use(express.json());
+app.use(cors());
+
+//set up a port
+const PORT = 8080;
+
+app.listen(PORT, () => {
+  console.info(`Server is running in port ${PORT}`);
+});
+
+//root route
+//GET --> READ
+app.get("/", (_, res) => {
+  //   res.json({ message: "Welcome to the server. GET comfy" });
+  res.send("Welcome!");
+});
