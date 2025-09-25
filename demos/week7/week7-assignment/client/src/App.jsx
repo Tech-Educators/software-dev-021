26b4c230c5236b65a7d8fb39894826ad6fc3a49a
Manuel <manugonzalez.tech@gmail.com>
2025-09-25 15:10:52 +0100

    feat: add week 7 assignment template

---
+++ demos/week7/week7-assignment/client/src/App.jsx
@@ -0,0 +1,2 @@
+// TODO: We set up routes in here. (Routes, Route)
+//- All our components will be rendered in here
---
+++ demos/week7/week7-assignment/client/src/components/Content.jsx
@@ -0,0 +1,8 @@
+//TODO: render the database data sent through the server
+// - store data in state
+// - fetch the values from the server --> this is an effect
+// - once you fetch teh data, parse it to JSON --> console log it!
+// - update state with the fetched data (maybe some wrangling?)
+// - poll your database to keep your data updated --> this is an effect
+// - render the list of content
+//? - you could conditionally render some elements in your page
